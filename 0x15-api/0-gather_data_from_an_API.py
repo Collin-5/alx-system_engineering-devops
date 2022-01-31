5f412b1e3d0c0099b25f07a496aa2df0f2bf1c94
Collin-5 <collins380@gmail.com>
2022-01-31 11:36:00 +0300

    api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+requesting data from an api
+"""
+import requests
+from sys import argv
+
+
+def show():
+    users = requests.get('https://jsonplaceholder.typicode.com/users')
+    for user in users.json():
+        if user.get('id') == int(argv[1]):
+            EMPLOYEE_NAME = user.get('name')
+            break
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    NUMBER_OF_DONE_TASKS = 0
+    TOTAL_NUMBER_OF_TASKS = 0
+    TASK_TITLE = []
+    for todo in todos.json():
+        if todo.get('userId') == int(argv[1]):
+            TOTAL_NUMBER_OF_TASKS += 1
+            if todo.get('completed') is True:
+                NUMBER_OF_DONE_TASKS += 1
+                TASK_TITLE.append(todo.get('title'))
+    print("Employee {} is done with tasks({}/{}):".format(EMPLOYEE_NAME,
+                                                          NUMBER_OF_DONE_TASKS,
+                                                          TOTAL_NUMBER_OF_TASKS))
+
+    for task in TASK_TITLE:
+        print(f'\t{task}')
+
+
+if __name__ == '__main__':
+    show()

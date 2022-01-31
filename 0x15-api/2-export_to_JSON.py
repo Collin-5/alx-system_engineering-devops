586142777448ab4e6da991dc3d2d4dce4ef261de
Collin-5 <collins380@gmail.com>
2022-01-31 23:58:05 +0300

    to json

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,37 +1,35 @@
 #!/usr/bin/python3
 """
-requesting data from an api
-export to json
+Request from API; Return TODO list progress given employee ID
+Export this data to JSON
 """
+from sys import argv
 import json
 import requests
-from sys import argv
 
 
 def to_json():
-    """export json"""
-    users = requests.get('https://jsonplaceholder.typicode.com/users')
-    for user in users.json():
-        if user.get('id') == int(argv[1]):
-            EMPLOYEE_NAME = user.get('name')
+    """return API data"""
+    users = requests.get("http://jsonplaceholder.typicode.com/users")
+    for u in users.json():
+        if u.get('id') == int(argv[1]):
+            USERNAME = (u.get('username'))
             break
+    TASK_STATUS_TITLE = []
+    todos = requests.get("http://jsonplaceholder.typicode.com/todos")
+    for t in todos.json():
+        if t.get('userId') == int(argv[1]):
+            TASK_STATUS_TITLE.append((t.get('completed'), t.get('title')))
 
-    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
-    TASK_COMPLETED_STATUS = []
-    for todo in todos.json():
-        if todo.get('userId') == int(argv[1]):
-            TASK_COMPLETED_STATUS.append((todo.get('completed'),
-                                          todo.get('title')))
-
-    inside = []
-    for status in TASK_COMPLETED_STATUS:
-        inside.append({"task": status[1],
-                       "completed": status[0],
-                       "username": EMPLOYEE_NAME})
-    data = {str(argv[1]): inside}
+    """export to json"""
+    t = []
+    for task in TASK_STATUS_TITLE:
+        t.append({"task": task[1], "completed": task[0], "username": USERNAME})
+    data = {str(argv[1]): t}
     filename = "{}.json".format(argv[1])
     with open(filename, "w") as f:
         json.dump(data, f)
 
-if __name__ == '__main__':
-    to_json()
+
+if __name__ == "__main__":
+    to_json

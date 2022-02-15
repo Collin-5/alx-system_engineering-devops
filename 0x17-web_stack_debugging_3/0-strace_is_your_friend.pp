3fe8e251844bb9a2c9f2d844996dace06c88615a
Collin-5 <collins380@gmail.com>
2022-02-15 21:47:40 +0300

    fix typo

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# error with file extention
+
+exec { 'fix config typo':
+  command => "sed -i 's/.phpp/.php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
